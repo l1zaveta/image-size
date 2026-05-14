6fae11605ba2e64aba3afabd021ecd99be133765
Elizaveta Krasnoperova <144271794+l1zaveta@users.noreply.github.com>
2026-05-14 21:20:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,18 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const storage = multer.memoryStorage();
-const upload = multer({ storage: storage, limits: { fileSize: 10 * 1024 * 1024 } });
-
+const upload = multer({
+    storage: storage,
+    fileFilter: (req, file, cb) => {
+        
+        if (file.mimetype === 'image/png') {
+            cb(null, true);
+        } else {
+            cb(new Error('Только PNG'), false);
+        }
+    },
+    limits: { fileSize: 10 * 1024 * 1024 }
+});
 
 app.get('/login/', (req, res) => {
     res.type('text/plain');
@@ -19,10 +29,21 @@ app.post('/size2json/', upload.single('image'), async (req, res) => {
     }
     try {
         const metadata = await sharp(req.file.buffer).metadata();
-        return res.json({ width: metadata.width || 0, height: metadata.height || 0 });
+        return res.json({
+            width: metadata.width || 0,
+            height: metadata.height || 0
+        });
     } catch (error) {
         return res.json({ width: 0, height: 0 });
     }
+});
+
+
+app.use((err, req, res, next) => {
+    if (err) {
+        return res.json({ width: 0, height: 0 });
+    }
+    next();
 });
 
 app.listen(port, '0.0.0.0', () => {
